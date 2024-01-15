14b296f038211989efa1433941eaa2c3f797cb6b
Gautam Roy <zademathews@gmail.com>
2024-01-15 23:43:53 +0000

    1768. Merge Strings Alternately

---
+++ 1768_merge_strings_alternately.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        word1 = list(word1)
+        word2 = list(word2)
+
+        result = ""
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result += word1[i]
+            if i < len(word2):
+                result += word2[i]
+            i += 1
+        return result
+
+
+sol = Solution()
+print(sol.mergeAlternately("abc", "123444"))
