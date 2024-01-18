cb35e8bfc9392dff311ec21bcd9fef4a458c1755
Gautam Roy <zademathews@gmail.com>
2024-01-18 04:58:03 +0000

    459. Repeated Substring Pattern

---
+++ 459_Repeated_Substring_Pattern.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return (s + s)[1:-1].find(s) != -1
+
+
+result = Solution().repeatedSubstringPattern("abcabc")
+print(result)
