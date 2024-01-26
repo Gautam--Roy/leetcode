7cc27f81ac3845c736b4d30820e150bfe0526513
Gautam Roy <zademathews@gmail.com>
2024-01-26 06:12:38 +0000

    657. Robot Return to Origin - Fastest Solution

--- 657_Robot_Return_to_Origin.py
+++ 657_Robot_Return_to_Origin.py
@@ -4,28 +4,10 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        
-        R = [0, 1]
-        L=  [0 , -1]
-        U = [1, 0]
-        D = [-1, 0]
-        P = [0,0]
-        for m in range(len(moves)):
-            match moves[m]:
-                case "R":
-                    P[1] += R[1]
-                case "L":
-                    P[1] += L[1]
-                case "U":
-                    P[0] += U[0]
-                case "D":
-                    P[0] += D[0]
-                case _:
-                    pass
-        if P == [0,0]:
+        # Fastest solution on leet
+        if moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R'):
             return True
-        else: return False
-        
+        return False
         
 result = Solution().judgeCircle("LL")
 print(result)
