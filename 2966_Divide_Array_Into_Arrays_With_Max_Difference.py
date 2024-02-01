81e5f645c3010626aa143cce4a8d96d1ccc3b712
Gautam Roy <zademathews@gmail.com>
2024-02-01 21:58:02 +0000

    2966. Divide Array Into Arrays With Max Difference

---
+++ 2966_Divide_Array_Into_Arrays_With_Max_Difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def divideArray(self, nums: [int], k: int) -> [[int]]:
+        sortedArr = sorted(nums)
+        ans = []
+        for oc in range(0, len(sortedArr), 3):
+            print(f" nimus : {sortedArr[oc + 2] - sortedArr[oc]}")
+            if sortedArr[oc + 2] - sortedArr[oc] <= k:
+                ans.append(sortedArr[oc : oc + 3])
+        if len(ans) != len(sortedArr) / 3 : return []
+        else: return ans
+result = Solution().divideArray([33,26,4,18,16,24,24,15,8,18,34,20,24,16,3], 16)
+print(result)
