c2a3b3a4d95a5f502a5f7dc40a54a20f43599ffb
Gautam Roy <zademathews@gmail.com>
2024-02-20 04:28:06 +0000

    268. Missing Number

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -1,8 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        sum = n * (n+1)/2
+        sum = n * (n + 1) / 2
         check = 0
-        for x in range(0 , n):
+        for x in range(0, n):
             check += nums[x]
         return int(sum - check)
