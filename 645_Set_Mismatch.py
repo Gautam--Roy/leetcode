48dfc3c30fe7969264a827f7ea20b7eedfe1a5e2
Gautam Roy <zademathews@gmail.com>
2024-02-24 06:27:23 +0000

    645. Set Mismatch

---
+++ 645_Set_Mismatch.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        total_sum = (n * (n + 1)) // 2
+        actual_sum = sum(nums)
+        duplicate = actual_sum - sum(set(nums))
+        missing = total_sum - actual_sum + duplicate
+
+        return [duplicate, missing]
