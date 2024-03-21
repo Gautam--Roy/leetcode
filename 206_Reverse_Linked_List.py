01cd5df23e8a98522adbb5c6157240e3099f8155
Gautam Roy <zademathews@gmail.com>
2024-03-21 02:53:56 +0000

    206. Reverse Linked List

--- 206_Reverse_Linked_List.py
+++ 206_Reverse_Linked_List.py
@@ -1,3 +1,23 @@
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         prev, curr = None, head
+
+#         while curr:
+#             next = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next
+
+#         return curr
+
+# # This is interative method
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,14 +25,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
-        
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        
-        return curr
-    
-# This is interative method 
+        prev = None
+        current = head
+
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev
