2f1f44dc395044e5af40988e136ad2698cbefc73
Gautam Roy <zademathews@gmail.com>
2024-04-01 04:26:54 +0000

    58. Length of Last Word - Submission 2

--- 58_Length_of_Last_Word.py
+++ 58_Length_of_Last_Word.py
@@ -1,12 +1,19 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+# class Solution(object):
+#     def lengthOfLastWord(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
 
-        return len(s.split()[-1])
+#         return len(s.split()[-1])
 
 
-result = Solution().lengthOfLastWord("Hello World ")
-print(result)
+# result = Solution().lengthOfLastWord("Hello World ")
+# print(result)
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        s = s.split(" ")
+        return len(s[-1])
