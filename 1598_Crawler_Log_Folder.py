e99193967b170c7355ab07782072b2459df9a690
Gautam Roy <zademathews@gmail.com>
2024-07-10 18:43:04 +1200

    1598. Crawler Log Folder

---
+++ 1598_Crawler_Log_Folder.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        d = 0
+        for i in logs:
+            if i != "../" and i != "./":
+                d += 1
+            elif i == "../" and d != 0:
+                d -= 1
+        return d
