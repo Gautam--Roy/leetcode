e0f57e422f25bc2563ddba88b6e3c2076ccfc8cc
Gautam Roy <zademathews@gmail.com>
2024-08-15 14:20:06 +1200

    860. Lemonade Change

--- 860_Lemonade_Change.py
+++ 860_Lemonade_Change.py
@@ -21,5 +21,23 @@ result = Solution().lemonadeChange([5,5,5,10,20])
 print(result)
             
                     
-        
-        
+# date : 15thAugust2024       
+ # class Solution:
+ #    def lemonadeChange(self, bills: List[int]) -> bool:
+ #        tens = 0
+ #        fives = 0
+ #
+ #        for bill in bills:
+ #            if bill == 5:
+ #                fives += 1
+ #            if bill == 10:
+ #                tens += 1
+ #                fives -= 1
+ #            if bill == 20:
+ #                if tens > 0: 
+ #                    tens -= 1
+ #                    fives -= 1
+ #                else:
+ #                    fives -= 3
+ #            if fives < 0: return False
+ #        return True       
