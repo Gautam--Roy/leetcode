9499fa47ee3116ea7885faba9459f9a45e4f6b2a
Gautam Roy <zademathews@gmail.com>
2024-10-23 21:21:48 +0530

    2641. Cousins in Binary Tree II

---
+++ 2641_Cousins_in_Binary_Tree_II.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def replaceValueInTree(self, root: TreeNode) -> TreeNode:
+        q = deque([root])
+        level_sum = []
+        while q:
+            level_size = len(q)
+            level_total = 0
+            for _ in range(level_size):
+                node = q.popleft()
+                level_total += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level_sum.append(level_total)
+        q.append(root)
+        root.val = 0
+        level = 0
+
+        while q:
+            level_size = len(q)
+            next_level_sum = level_sum[level + 1] if level + 1 < len(level_sum) else 0
+
+            for _ in range(level_size):
+                node = q.popleft()
+                child_sum = 0
+
+                if node.left:
+                    child_sum += node.left.val
+                    q.append(node.left)
+                if node.right:
+                    child_sum += node.right.val
+                    q.append(node.right)
+
+                if node.left:
+                    node.left.val = next_level_sum - child_sum
+                if node.right:
+                    node.right.val = next_level_sum - child_sum
+
+            level += 1
+
+        return root
